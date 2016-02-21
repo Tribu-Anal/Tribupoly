4c6f424d6f98bee7d7fbfc8e322acf16a9e15409
wupa9 <wupaz9@gmail.com>
2016-02-21 15:31:09 -0600

    Game start validations

---
+++ js/app.js
@@ -0,0 +1,24 @@
+"use strict";
+
+$('#start').click(function() {
+    let namePlayer1 = $('#p1name').val(),
+        namePlayer2 = $('#p2name').val();
+    
+    if (namePlayer1.length === 0 || namePlayer2.length === 0) {
+        alert("You must enter your names!");
+        return;
+    }
+    
+    let tokenPlayer1 = $('#tokens1 input').val(),
+        tokenPlayer2 = $('#tokens2 input').val();
+    
+    if (tokenPlayer1 === tokenPlayer2) {
+        alert("You must choose different tokens!");
+        return;
+    }
+    
+    selectToken(tokenPlayer1, namePlayer1);
+    selectToken(tokenPlayer2, namePlayer2);
+    
+    //Aqui llamare tu funcion para cambiar el div
+});
